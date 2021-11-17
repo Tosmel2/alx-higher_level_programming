c8b664813e5de8f8646507abe590bfe66761b588
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-11-17 21:39:40 +0100

    Delete 9-cities_by_states_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_states_join.sql
+++
@@ -1,4 +0,0 @@
--- select all cities and display state information using join
--- select all cities and display state information using join
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states
-ON cities.state_id=states.id;
