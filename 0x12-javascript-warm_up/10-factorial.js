a67419566137ee7c4ce4873746e50cf3cc29f032
Tosmel2 <Adewaletosin0808@gmail.com>
2021-12-13 11:54:49 -0800

    javascript warm up

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let number = parseInt(process.argv[2]);
+function factorial (num) {
+  if (num > 1) {
+     return num * factorial(num - 1);
+   } else {
+     return 1;
+   }
+}
+console.log(factorial(number));
+
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+
+#!/usr/bin/node
+const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let lang in langs) {
+  console.log(langs[lang]);
+}
+
