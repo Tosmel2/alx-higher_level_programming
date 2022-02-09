809342f200534debcf69c179dd0bc8e76aa398c9
Tosmel2 <Adewaletosin0808@gmail.com>
2022-02-09 07:09:59 -0800

    Readme

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('HEADER');
+  header.style.color = '#FF0000';
+});
+
---
+++ 0x15-javascript-web_jquery/README.md
@@ -0,0 +1,2 @@
+0x15. Javascript - Web JQuery
+	This project is an introduction to web JQuery and its goal is to learn how to manipulate DOM elements with javascript.
