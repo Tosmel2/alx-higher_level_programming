e644229624eca7f347626bb3cd9c1ec97281d016
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2022-03-28 14:27:03 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-        import sys
-        result = 0
-        for i in range(1, len(sys.argv)):
-             result += int(sys.argv[i])
-        print("{}".format(result))
-
+    import sys
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print("{}".format(result))
