4142e4ef91bf14ea3e6cc989592b0c0f8e3b2109
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2022-03-28 14:09:41 +0100

    Update 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,8 +3,7 @@ def remove_char_at(str, n):
     new = ''
     i = 0
     for c in str:
-         if i != n:
-             new += str[i]
-         i += 1
-     return new
-
+        if i != n:
+            new += str[i]
+        i += 1
+    return new
