0fbd36b71dec440a1edcb3e8359dbe3f66da3d45
“tosmel2” <adewaletosin0808@gmail.com>
2022-06-20 22:56:12 +0100

    python everything_is_object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(listy=[]):
-    listy += ["Holberton"]
-    return ", ".join(listy)
+def magic_string(count=[-1]):
+    count[0] += 1
+    return "Holberton" + ", Holberton" * count[0]
